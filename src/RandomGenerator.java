337a1e42d520e67a039e04b690a7f61a21a2b0de
felipe993 <feliperosabal93@gmail.com>
2017-02-17 12:41:42 -0600

    generador uniforme y exponencial

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -1,5 +1,18 @@
 /**
  * Created by felipe on 17/2/2017.
  */
+import java.util.Random;
+import java.lang.Math;
 public class RandomGenerator {
+    Random r = new Random();
+    public double uniform(double a, double b){
+
+        return a+(b-a)*r.nextDouble();
+    }
+    public double normal(int u, int o){
+        return 0;
+    }
+    public double exponential(double l){
+        return 1-Math.pow(Math.E, l*r.nextDouble());
+    }
 }
