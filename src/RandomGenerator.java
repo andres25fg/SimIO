ae11be46c8b5738685fd11f089c3c19d4c8b7d16
felipe993 <feliperosabal93@gmail.com>
2017-02-17 16:45:57 -0600

    arregla errores del generador de aleatorio

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -6,13 +6,12 @@ import java.lang.Math;
 public class RandomGenerator {
     Random r = new Random();
     public double uniform(double a, double b){
-
         return a+(b-a)*r.nextDouble();
     }
     public double normal(int u, int o){
         return 0;
     }
     public double exponential(double l){
-        return 1-Math.pow(Math.E, l*r.nextDouble());
+        return -1/l*Math.log(r.nextDouble());
     }
 }
