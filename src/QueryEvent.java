23059ed83b2193a226c508eef020706d7e061139
felipe993 <feliperosabal93@gmail.com>
2017-02-19 16:56:48 -0600

    crea  metodos de la clase QueryEvent

--- src/QueryEvent.java
+++ src/QueryEvent.java
@@ -4,10 +4,34 @@
 
 //enum con los tipos de eventos
  enum EventType {
-    CONECTION_IN(),
-    CONNECTION_OUT(),
-    TIME_OUT(),
-    EXIT_MODULE();
+    CONECTION_IN("connection_in"),
+    CONNECTION_OUT("connection_out"),
+    TIME_OUT("time_out"),
+    EXIT_MODULE("exit_Module");
+
+    private String type;
+
+    EventType(String type){
+        this.type=type;
+    }
+
+    public String getType(){
+        return type;
+    }
 }
 public class QueryEvent {
+     private  double eventTime=0;
+     private  EventType type;
+
+    QueryEvent(int time, EventType type){
+        eventTime= time;
+        this.type = type;
+    }
+
+    public String getType(){
+        return type.getType();
+    }
+    public double getEventTime(){
+        return eventTime;
+    }
 }
