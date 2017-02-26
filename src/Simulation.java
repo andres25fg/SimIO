2a8d4e741c386c683cdc3df39741258d32db8efa
felipe993 <feliperosabal93@gmail.com>
2017-02-26 16:32:23 -0600

    cambios que no pude poner por un conflicto en el main

--- src/Simulation.java
+++ src/Simulation.java
@@ -347,6 +347,15 @@ public class Simulation  {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
+
+        //prueba
+        int secSim=100;
+        int numSim =1;
+        boolean slowMode=false;
+        int slowModeSecs=0;
+        Simulation simulation = new Simulation(numSim, secSim,slowMode,slowModeSecs);
+
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
