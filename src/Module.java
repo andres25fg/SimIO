aac46f12c2193136fd876f90af93242a9ecca8de
felipe993 <feliperosabal93@gmail.com>
2017-02-28 00:48:04 -0600

    quite un print que puse para una prueba

--- src/Module.java
+++ src/Module.java
@@ -157,7 +157,6 @@ public abstract class Module {
         if(c.getStack()){
             //si entro a la cola al tiempo de servicio se le suma el tiempo que paso en la cola y se coloca
             serviceTime += clock-c.getStackArrivalTime();
-            System.out.println("sdsdfsdfsdefsdf"+(clock-c.getStackArrivalTime()));
             c.setStack(false);
         }
         switch (c.getType().toString()){
